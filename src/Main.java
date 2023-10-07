f7f84074480d0567b318d2ae83a12ca43f079900
n00ra666 <noora.kojonen@hotmail.com>
2023-10-07 16:19:25 +0300

    Ensimmäisen muuttujan määrittely, ohjelman nimen ja tekijän tulostaminen konsoliin

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String tekija = "Noora";
+		
+		System.out.println("Hei olen Tulostin-ohjelma");
+		System.out.println("Ohjelman tekijä: " + tekija);
 	}
 
 }
