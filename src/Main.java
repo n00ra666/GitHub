6b25af36038e1974bba6931668c8444731aafc74
n00ra666 <noora.kojonen@hotmail.com>
2023-10-07 16:23:27 +0300

    Numeromuuttujien alustaminen ja tulostaminen konsoliin

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,14 @@ public class Main {
 		
 		String tekija = "Noora";
 		
+		int luku1 = 3;
+		int luku2 = 4;
+		
 		System.out.println("Hei olen Tulostin-ohjelma");
 		System.out.println("Ohjelman tekijä: " + tekija);
+		
+		System.out.println("Luku1-muuttujan arvo on " + luku1 + ".");
+		System.out.println("Luku2-muuttujan arvo on " + luku2 + ".");
 	}
 
 }
